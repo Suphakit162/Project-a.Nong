7172cfc0e2ed260c2022138809c90fb3dbfc5967
Suphakit162 <supakita1234@gmail.com>
2025-06-16 22:26:00 +0700

    เเก้หน้า login ให้ redirect ไปหน้า index.html หลังกด login

--- furni-1.0.0/backendServer/app.js
+++ furni-1.0.0/backendServer/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 const productRoutes = require('./routes/products');
@@ -10,6 +11,12 @@ const loginData = require('./data/login');
 
 app.use(cors());
 app.use(express.json());
+
+//เสิร์ฟทุกไฟล์ static (เช่น index.html, login.html) จากโฟลเดอร์ furni-1.0.0
+app.use(express.static(path.join(__dirname, '..')));
+console.log('Static root folder is:', path.join(__dirname, '..'));
+
+
 
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
