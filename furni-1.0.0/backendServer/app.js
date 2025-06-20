2eb466baffd539f4f2eb29f9bfb1692578ed54ce
Pherawich-T <Pherawichtae@gmail.com>
2025-06-20 21:27:41 +0700

    20 May 2025
    -Modify shop.html⚠️

--- furni-1.0.0/backendServer/app.js
+++ furni-1.0.0/backendServer/app.js
@@ -7,15 +7,29 @@ const cartRoutes = require('./routes/cart');
 const ordersRoutes = require('./routes/orders');
 const registerData = require('./data/register');
 const loginData = require('./data/login');
+const helmet = require('helmet');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "'unsafe-inline'", "https:"],
+    styleSrc: ["'self'", "'unsafe-inline'", "https:"],
+    fontSrc: ["'self'", "https:"],
+    imgSrc: ["'self'", "data:", "https:"],
+    connectSrc: ["'self'", "http://localhost:3000"],
+  }
+}));
 
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/orders', ordersRoutes);
 app.use('/api/register', registerData);
 app.use('/api/login', loginData);
+app.use('/images', express.static('images'));
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
